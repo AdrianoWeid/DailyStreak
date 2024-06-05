8c425124902edaf361268ab14576c7c6795b73d5
Adriano Weid <adrianoweid123@gmail.com>
2024-06-05 15:58:30 +0200

    update

---
+++ app/addHabit.js
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import { View, Text } from 'react-native';
+import { globalStyles } from 'styles';
+
+export default function SettingsTab() {
+  return (
+    <View style={globalStyles.container}>
+      <Text style={globalStyles.text}></Text>
+    </View>
+  );
+}
