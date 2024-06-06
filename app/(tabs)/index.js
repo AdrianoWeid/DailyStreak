6b4e05ff4151e2e6bdba1fc324e3c06795bb3c94
AdrianoWeid <adrianoweid123@gmail.com>
2024-06-07 00:19:47 +0200

    üpdate

--- app/(tabs)/index.js
+++ app/(tabs)/index.js
@@ -1,10 +1,21 @@
-import React, { useEffect, useState } from 'react';
-import { ScrollView, StyleSheet, Text, Button } from 'react-native';
+import React from 'react';
+import { ScrollView, StyleSheet } from 'react-native';
+import CalendarHeatmap from '@hackclub/react-calendar-heatmap';
+import '@hackclub/react-calendar-heatmap/dist/styles.css';
+
 
 export default function HomeTab() {
-
   return (
-    <ScrollView style={styles.container}>
+    <ScrollView style={styles.container}> {/* Ändere styles zu style */}
+      <CalendarHeatmap
+        endDate={new Date()}
+        values={[
+          { date: '2024-01-01', count: 12 },
+          { date: '2024-01-22', count: 122 },
+          { date: '2024-01-30', count: 38 },
+          // ...and so on
+        ]}
+      />
     </ScrollView>
   );
 }
--- app/components/ActivityCalendar.js
+++
@@ -1,8 +0,0 @@
-import React, {useState, useEffect } from 'react';
-import {
-    SQUARE_SIZE,
-    DAYS_IN_WEEK,
-    MONTH_LABEL_GUTTER_SIZE,
-    MONTH_LABELS,
-} from "./utils/constants"
-
